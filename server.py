ba743d4e131a3d46ca25c81e517c21a6d636c5bf
kevlar <kevin@pita.ninja>
2016-12-23 15:05:19 +0100

    Document server.py
    Signed-off-by: kevlar <kevin@pita.ninja>

--- server.py
+++ server.py
@@ -6,10 +6,10 @@ from socket import *
 from rsa import *
 from diffieHellman import *
 
-HOST = '127.1'
+HOST = '127.1' #127.1 = 127.0.0.1
 PORT = 4242
 
-def readkey(fn):
+def readkey(fn): #Function that read key from the file
     f = open(fn, 'r')
     e = int(f.readline())
     n = int(f.readline())
@@ -22,8 +22,10 @@ def main():
             help='public key with default name servPub.key')
     parser.add_argument('--priv-key', dest='privkey', default='servPriv.key',
             help='private key with default name servPriv.key')
-    parser.add_argument('--size', dest='keylen', default='1024', type=int,
-            help='size of the key with default value 1024')
+    parser.add_argument('--cli-key', dest='clikey', default='cliPub.key',
+            help='public key with default name cliPub.key')
+    parser.add_argument('--size', dest='keylen', default='2048', type=int,
+            help='size of the key with default value 2048')
 
     args = parser.parse_args()
 
@@ -43,35 +45,78 @@ def main():
     conn, addr = s.accept()
     print('Connected by ')
     print(addr)
-    e = int(conn.recv(4096).decode('utf-8'))
-    n = int(conn.recv(4096).decode('utf-8'))
-    cliPub = e, n
-    print('Received the client public key')
+    e = int(conn.recv(4096).decode('utf-8')) #received e
+    n = int(conn.recv(4096).decode('utf-8')) #received n
+
+    try: #if client public key is defined in parameters
+        cliPub = readkey(args.clikey)
+    except FileNotFoundError:
+        cliPub = e, n
     time.sleep(1)
-    conn.send(str(servPub[0]).encode('utf-8'))
+    conn.send(str(servPub[0]).encode('utf-8')) #send e
     time.sleep(1)
-    conn.send(str(servPub[1]).encode('utf-8'))
+    conn.send(str(servPub[1]).encode('utf-8')) #send n
 
     print('Key exchange for Diffie-Hellman')
-    a = int(conn.recv(4096).decode('utf-8'))
-    p = int(conn.recv(4096).decode('utf-8'))
-    g = int(conn.recv(4096).decode('utf-8'))
-    r = generatePrivate()
-    b = generatePart(p, g, n)
-    k = generateKey(a, n, p)
-    print('k = ', k)
-    conn.send(str(b).encode('utf-8'))
 
+    #Receiving A
+    a = s.recv(4096)
+    a = decrypt(servPriv, a)
+    h, a = check(cliPub, a)
+    if (h == True):
+        a = int(a.decode('utf-8'))
+    else:
+        a = 0
+        print('Warning the signature contain error')
+
+    #Receiving p
+    p = s.recv(4096)
+    p = decrypt(servPriv, p)
+    if (h == True):
+        p = int(p.decode('utf-8'))
+    else:
+        p = 0
+        print('Warning the signature contain error')
+
+    #Receiving g
+    g = s.recv(4096)
+    g = decrypt(servPriv, g)
+    if (h == True):
+        g = int(g.decode('utf-8'))
+    else:
+        g = 0
+        print('Warning the signature contain error')
+
+    #Generating the private number
+    r = generatePrivate() #random private number
+    #Generating B
+    b = generatePart(p, g, n) #b = part to send to other member
+    #Generating the key
+    k = generateKey(a, n, p) #k = the diffie-hellman generated key
+    print('k = ', k)
+    #sending B
+    b = sign(servPriv, str(b).encode('utf-8')) #sign B
+    b = encrypt(cliPub, b) #encrypt B
+    conn.send(b)
+
+    #Start the chat
     while True:
+        #Receiving client message
         data = conn.recv(4096)
         if (len(data) == 0):
             break
         data = decrypt(servPriv, data)
-        print('Received: ', data.decode('utf-8'))
+        h, m = check(cliPub, data)
+        if (h == True):
+            print('Received: ', m.decode('utf-8'))
+        else:
+            print('The message signature contain error!')
+            print('Received: ', m.decode('utf-8'))
+        #Sending a message to the client
         reply = input('Reply: ')
         if (reply == 'exit'):
             break
-        reply = reply.encode('utf-8')
+        reply = sign(servPriv, reply.encode('utf-8'))
         reply = encrypt(cliPub, reply)
         conn.sendall(reply)
 
